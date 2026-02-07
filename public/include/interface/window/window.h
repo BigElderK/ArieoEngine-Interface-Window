e258deda4d5050a9b798622d2c4d1d745798a5a6
BigElderK <BigElderK@divineland.org>
2026-02-07 17:30:34 +0800

    init

---
+++ public/include/interface/window/window.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "interface/main/main_module.h"
+namespace Arieo::Interface::Window
+{
+    class IWindowManager;
+    class IWindow
+    {
+    public:
+        virtual std::uint64_t getWindowHandle() = 0;
+        virtual Base::Math::Rect<std::uint32_t> getWindowRect() = 0;
+        virtual Base::Math::Vector<std::uint32_t, 2> getFramebufferSize() = 0;
+        virtual IWindowManager* getWindowManager() = 0;
+        virtual bool isClosed() = 0;
+        virtual Base::StringID getWindowPlatform() = 0;
+    };
+
+    class IWindowManager
+        : public Arieo::Interface::Main::ITickable
+    {
+    public:
+        virtual void* getDisplay() = 0;
+        virtual IWindow* createWindow(std::uint16_t pos_x, std::uint16_t pos_y, std::uint16_t width, std::uint16_t height) = 0;
+        virtual void destroyWindow(IWindow*) = 0;
+
+        virtual IWindow* getMainWindow() = 0;
+    };
+}
